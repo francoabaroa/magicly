921a18f76954b11dd938c48ebec9a5b41da411d6
Franco Abaroa <franco9395@gmail.com>
2020-08-04 13:37:47 -0400

    Initial project commit

---
+++ packages/clientOld/src/pages/support.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function SupportPage() {
+  return(
+    <div>
+      <h1>Support Page!</h1>
+      <Link href="/"><a>Home</a></Link>
+    </div>
+  )
+}
