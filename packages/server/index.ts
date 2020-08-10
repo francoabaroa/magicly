7bac102a150a541feeec5fe1babc2c5e28e6d6aa
Franco Abaroa <franco9395@gmail.com>
2020-08-10 17:01:30 -0400

    implement new server logic for typescript/sequelize support

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -5,7 +5,21 @@ import morgan from 'morgan';
 import nextApp from '@magicly/client';
 import apolloServer from '@magicly/graphql';
 
+import { Request, Response } from 'express';
+import { createModels } from './models/index';
+import { HomeworkInstance } from './models/Homework';
+import { UserInstance } from './models/User';
+
 const { PORT } = process.env;
+const sequelizeConfig = {
+  'database': process.env.DATABASE_TEST || process.env.DATABASE,
+  'username': process.env.DATABASE_USER,
+  'password': process.env.DATABASE_PASSWORD,
+  'params': {
+    dialect: 'postgres',
+    operatorsAliases: false
+  }
+};
 
 async function main() {
   const app = express();
@@ -15,10 +29,28 @@ async function main() {
   await bootstrapApolloServer(app);
   await bootstrapClientApp(app);
 
-  app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`[ server ] ready on port ${PORT}`);
-  });
+  const isTest = !!process.env.DATABASE_TEST;
+  const isProduction = !!process.env.DATABASE_URL;
+
+  const db = createModels(sequelizeConfig);
+  db.sequelize.sync({ force: isTest || isProduction }).then(async () => {
+    if (isTest || isProduction) {
+      db.User.create({
+        currentCity: 'Miami',
+        hasSocialAuthLogin: false,
+        email: 'franco@franco.com',
+      }).then(user => {
+        console.log(user);
+      }).catch(err => {
+        console.error(err);
+      });
+    }
+
+    app.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`[ server ] ready on port ${PORT}`);
+    });
+  })
 }
 
 async function bootstrapClientApp(expressApp) {
