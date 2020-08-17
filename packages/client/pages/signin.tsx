36e1722bd806e69f37ad3fb3eec6d8c9fbc9a6e0
Franco Abaroa <franco9395@gmail.com>
2020-08-17 15:56:53 -0400

    add sign up and sign in capabilities

--- packages/client/pages/signin.tsx
+++ packages/client/pages/signin.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import Layout from '../components/Layout';
+import useSignInForm from '../components/SignInForm';
 import gql from 'graphql-tag';
 import { withApollo } from '../apollo/apollo';
 
@@ -19,25 +22,51 @@ const SIGN_IN = gql`
 `;
 
 const SignInPage = () => {
+  const router = useRouter();
+  // TODO: show meaningful message if account doesnt exist
+  const signin = () => {
+    signIn({
+      variables: {
+        email: inputs.email,
+        password: inputs.password,
+      }
+    });
+  };
+
+  const { inputs, handleInputChange, handleSubmit } = useSignInForm({ email: '', password: '' }, signin);
   const [signIn, { data, loading, error }] = useMutation(SIGN_IN);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  if (data && data.signIn && data.signIn.token) {
+    localStorage.setItem('token', data.signIn.token);
+    router.push('/main', undefined, { shallow: true });
+  }
+
   return (
     <Layout>
       <h1>SignIn Page</h1>
       <pre> Mutation Data: {JSON.stringify(data)}</pre>
-      <button
-        onClick={() => signIn({
-          variables: {
-            email: 'franco@franco.com',
-            password: 'testa123',
-          }
-        })}
-      >
-        Sign In
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div>
+          <label>Email Address</label>
+          <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" onChange={handleInputChange} value={inputs.password} autoComplete="on" required />
+        </div>
+        <button type="submit">
+          Sign In
       </button>
+      </form>
+      <h4>
+        Don't have an account?
+        <Link href="/signup">
+          <a>Sign Up</a>
+        </Link>
+      </h4>
     </Layout>
   );
 };
--- packages/client/pages/signup.tsx
+++ packages/client/pages/signup.tsx
@@ -1,6 +1,9 @@
+import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 import { useMutation } from '@apollo/react-hooks';
 import Layout from '../components/Layout';
+import useSignUpForm from '../components/SignUpForm';
 import gql from 'graphql-tag';
 import { withApollo } from '../apollo/apollo';
 
@@ -42,36 +45,74 @@ const SIGN_UP = gql`
 `;
 
 const SignUpPage = () => {
+  const router = useRouter();
+  // TODO: check if pw1 === pw2
+  // TODO: need node service to check if email is valid by sending confirmation email.
+  // TODO: show meaningful message if user email already exists
+  const signup = () => {
+    signUp({
+      variables: {
+        email: inputs.email,
+        currentCity: inputs.city,
+        hasSocialAuthLogin: false,
+        password: inputs.password1,
+        firstName: null,
+        lastName: null,
+        displayName: null,
+        socialAuthId: null,
+        preferredSocialAuth: null,
+        salt: null,
+        gender: null,
+        cellphone: null,
+        dob: null,
+      }
+    });
+  };
+
   const [signUp, { data, loading, error }] = useMutation(SIGN_UP);
+  const { inputs, handleInputChange, handleSubmit } = useSignUpForm({ email: '', city: '', password1: '', password2: '' }, signup);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (error && error.message.includes('SequelizeUniqueConstraintError')) {
+    return <p>Error: {'This email already exists in our system. Please reset your password.'}</p>
+  }
+
+  if (data && data.signUp && data.signUp.token) {
+    localStorage.setItem('token', data.signUp.token);
+    router.push('/main', undefined, { shallow: true })
+  }
 
   return (
     <Layout>
       <h1>SignUp Page</h1>
       <pre> Mutation Data: {JSON.stringify(data)}</pre>
-      <button
-        onClick={() => signUp({
-          variables: {
-            email: 'ale@ale.com',
-            currentCity: 'Dallas',
-            hasSocialAuthLogin: false,
-            password: '50dfjdfwo',
-            firstName: 'Ale',
-            lastName: 'Aleee',
-            displayName: 'Ale',
-            socialAuthId: null,
-            preferredSocialAuth: null,
-            salt: null,
-            gender: null,
-            cellphone: '7864333444',
-            dob: null,
-          }
-        })}
-      >
-        Sign Up
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <div>
+          <label>Email Address</label>
+          <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+        </div>
+        <div>
+          <label>Current City</label>
+          <input type="text" name="city" onChange={handleInputChange} value={inputs.city} required />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password1" onChange={handleInputChange} value={inputs.password1} required />
+        </div>
+        <div>
+          <label>Re-enter Password</label>
+          <input type="password" name="password2" onChange={handleInputChange} value={inputs.password2} required />
+        </div>
+        <button type="submit">
+          Sign Up
       </button>
+      </form>
+      <h4>
+        Already have an account?
+        <Link href="/signin">
+          <a>Sign In</a>
+        </Link>
+      </h4>
     </Layout>
   );
 };
