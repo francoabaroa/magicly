0d519f181a7922b027df5a4f9e42940174ec93c9
Franco Abaroa <franco9395@gmail.com>
2020-09-04 13:40:26 -0400

    debug memory leak

--- packages/server/lib/uploaders/s3.ts
+++ packages/server/lib/uploaders/s3.ts
@@ -20,31 +20,31 @@ export class AWSS3Uploader implements ApolloServerFileUploads.IUploader {
   public config: S3UploadConfig;
 
   constructor(config: S3UploadConfig) {
-    // AWS.config = new AWS.Config();
-    // AWS.config.update({
-    //   region: config.region || process.env.AWS_DEFAULT_REGION ||'us-east-1',
+    AWS.config = new AWS.Config();
+    AWS.config.update({
+      region: config.region || process.env.AWS_DEFAULT_REGION ||'us-east-1',
 
-    //   accessKeyId: config.accessKeyId,
-    //   secretAccessKey: config.secretAccessKey
-    // });
+      accessKeyId: config.accessKeyId,
+      secretAccessKey: config.secretAccessKey
+    });
 
-    // this.s3 = new AWS.S3();
-    // this.config = config;
+    this.s3 = new AWS.S3();
+    this.config = config;
   }
 
-  private createUploadStream(key: string, userId: any, mimetype: string): S3UploadStream {
-    const pass = new stream.PassThrough();
-    return {
-      writeStream: pass,
-      promise: this.s3
-        .upload({
-          Bucket: this.config.destinationBucketName + '/' + userId,
-          Key: key,
-          ContentType: mimetype,
-          Body: pass
-        })
-        .promise()
-    };
+  private createUploadStream(key: string, userId: any, mimetype: string): any {
+    // const pass = new stream.PassThrough();
+    // return {
+    //   writeStream: pass,
+    //   promise: this.s3
+    //     .upload({
+    //       Bucket: this.config.destinationBucketName + '/' + userId,
+    //       Key: key,
+    //       ContentType: mimetype,
+    //       Body: pass
+    //     })
+    //     .promise()
+    // };
   }
 
   private createDestinationFilePath(
@@ -76,33 +76,33 @@ export class AWSS3Uploader implements ApolloServerFileUploads.IUploader {
   async singleFileUploadResolver(
     file: ApolloServerFileUploads.File,
     me: any
-  ): Promise<ApolloServerFileUploads.UploadedFileResponse> {
-    const { createReadStream, filename, mimetype, encoding } = await file;
-    const fileName = filename.split('.')[0];
-    const fileEnding = filename.split('.')[1];
-    const userId = me && me.id ? me.id : '';
-    const dateString = new Date().getTime();
-    const finalFileName =
-      userId + '_' + dateString + '_' + fileName + '.' + fileEnding;
-    const stream = createReadStream();
-    const filePath = this.createDestinationFilePath(
-      finalFileName,
-      mimetype,
-      encoding
-    );
-    const uploadStream = this.createUploadStream(filePath, me.id, mimetype);
-    stream.pipe(uploadStream.writeStream);
-    const result = await uploadStream.promise;
-    return { filename: finalFileName, mimetype, encoding, url: result.Location };
+  ): Promise<any> {
+    // const { createReadStream, filename, mimetype, encoding } = await file;
+    // const fileName = filename.split('.')[0];
+    // const fileEnding = filename.split('.')[1];
+    // const userId = me && me.id ? me.id : '';
+    // const dateString = new Date().getTime();
+    // const finalFileName =
+    //   userId + '_' + dateString + '_' + fileName + '.' + fileEnding;
+    // const stream = createReadStream();
+    // const filePath = this.createDestinationFilePath(
+    //   finalFileName,
+    //   mimetype,
+    //   encoding
+    // );
+    // const uploadStream = this.createUploadStream(filePath, me.id, mimetype);
+    // stream.pipe(uploadStream.writeStream);
+    // const result = await uploadStream.promise;
+    // return { filename: finalFileName, mimetype, encoding, url: result.Location };
   }
 
   async multipleUploadsResolver(
     parent,
     { files }: { files: ApolloServerFileUploads.File[] }
-  ): Promise<ApolloServerFileUploads.UploadedFileResponse[]> {
+  ): Promise<any> {
     // TODO: before using, change 0 to me.id
-    return Promise.all(
-      files.map(f => this.singleFileUploadResolver(f, 0))
-    );
+    // return Promise.all(
+    //   files.map(f => this.singleFileUploadResolver(f, 0))
+    // );
   }
 }
