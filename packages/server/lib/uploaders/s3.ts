0c6ef94d6c963579bf0ff1834cdd200d9699152c
Franco Abaroa <franco9395@gmail.com>
2020-09-04 14:46:07 -0400

    debug memory leak

--- packages/server/lib/uploaders/s3.ts
+++ packages/server/lib/uploaders/s3.ts
@@ -1,4 +1,4 @@
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 import stream from 'stream';
 import { ApolloServerFileUploads } from '../index';
 require('dotenv').config({ path: '../../../.env' });
