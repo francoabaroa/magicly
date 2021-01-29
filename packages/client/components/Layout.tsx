5a585ef1dc565e63aa0f7a21248adb33dab157c6
Franco Abaroa <franco9395@gmail.com>
2021-01-29 13:31:21 -0500

    add breadcrumbs

--- packages/client/components/Layout.tsx
+++ packages/client/components/Layout.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
 import AppBar from './AppBar';
 import { useQuery } from '@apollo/react-hooks';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import gql from 'graphql-tag';
 import { useRouter } from 'next/router';
-import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 
 const QUERY = gql`
   query GetMe {
@@ -16,8 +18,30 @@ const QUERY = gql`
   }
 `;
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    breadcrumbs: {
+      marginTop: '10px',
+      marginLeft: '10px',
+      fontFamily: 'Overpass, serif',
+      color: 'rgba(0, 38, 66, 0.5)'
+    },
+    breadcrumbTypography: {
+      fontFamily: 'Overpass, serif',
+      color: 'rgba(0, 38, 66, 0.5)'
+    },
+    breadLink: {
+      "&:visited": {
+        textDecoration: 'none',
+        color: 'rgba(0, 38, 66, 0.8)'
+      }
+    }
+  }),
+);
+
 const Layout = (props) => {
   const router = useRouter();
+  const classes = useStyles();
   const { data, loading, error, refetch } = useQuery(QUERY);
   let signedInUser = null;
 
@@ -29,11 +53,77 @@ const Layout = (props) => {
     signedInUser = null;
   }
 
+  const getCapitalizedString = (name: string) => {
+    const lowerCaseTitle = name.toLowerCase();
+    if (typeof lowerCaseTitle !== 'string') return ''
+    return lowerCaseTitle.charAt(0).toUpperCase() + lowerCaseTitle.slice(1)
+  };
+
+  const shouldShowBreadcrumbs = () => {
+    let homeRoute = '/home';
+    let productivityRoute = '/productivity';
+    let financeRoute = 'finance';
+
+    if (router.route.includes(homeRoute) && router.route !== homeRoute) {
+      return true;
+    }
+
+    if (router.route.includes(productivityRoute) && router.route !== productivityRoute) {
+      return true;
+    }
+
+    if (router.route.includes(financeRoute) && router.route !== financeRoute) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const getBreadcrumbs = () => {
+    let fullRoute = '';
+    let individualRoutes = router.route.split('/');
+    let breadcrumbChildren = [];
+
+    if (shouldShowBreadcrumbs()) {
+      individualRoutes.shift();
+      individualRoutes.forEach((route, index, routes) => {
+        fullRoute += '/' + route;
+
+        if (index === routes.length - 1) {
+          breadcrumbChildren.push(
+            <Typography color="textPrimary" key={index} className={classes.breadcrumbTypography}>
+              { getCapitalizedString(route) }
+            </Typography>
+          );
+        } else {
+          breadcrumbChildren.push(
+            <Link href={fullRoute} key={index}>
+              <a className={classes.breadLink}>
+                { getCapitalizedString(route) }
+              </a>
+            </Link>
+          );
+        }
+      });
+
+      return (
+        <Breadcrumbs aria-label="breadcrumb" className={classes.breadcrumbs}>
+          { breadcrumbChildren }
+        </Breadcrumbs>
+      );
+    }
+
+    return null;
+
+  };
+
   return (
-  <div>
+    <div>
       <AppBar signedInUser={signedInUser} />
-    {props.children}
-  </div>
-  )};
+      { getBreadcrumbs() }
+      {props.children}
+    </div>
+  )
+};
 
 export default Layout;
